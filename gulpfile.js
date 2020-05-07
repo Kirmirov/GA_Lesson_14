48a55499926def4fe1e6e4e8224b7c8fcf6b970a
Kirmirov <aruter@yandex.ru>
2020-05-07 03:57:40 +0300

    fixe: new ver

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,24 @@
 const {src, dest, watch} = require('gulp');
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass');
 const browserSync = require ('browser-sync').create();
+const sass = require('gulp-sass');
 
-exports.zcss = function cm () {
-  return src('./**/*.css')
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(dest('./css'));
-};
-
-exports.serve = function bs() {
-  servSass();
+function bs () {
+  serveSass();
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
   watch("./*.html").on('change', browserSync.reload);
-  watch("./sass/**/*.html", servSass);
+  watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
-function servSass() {
-  return src('./sass/*.scss')
+function serveSass() {
+  return src("./sass/*.sass")
     .pipe(sass())
-    .pipe(dest('./css'))
-    .pipe(browserSync.stream());
+    .pipe(dest("./css"))
+    .pipe(browserSync());
 };
 
+exports.serve = bs;
