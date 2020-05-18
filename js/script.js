c23a559986baf7c4706d41121fb8bbcdf6b17f36
Kirmirov <aruter@yandex.ru>
2020-05-18 05:49:49 +0300

    feat: added sections project and sixfoots with sliders, file sliders.sass with styles for sliders, wrote script for switch between titles

--- js/script.js
+++ js/script.js
@@ -24,8 +24,8 @@ $(document).ready(function() {
     }
   })
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 500) {
+  $(document).scroll(function() {
+    if ($(document).scrollTop() > 500) {
       scrollUp.addClass('scroll-up--show');
     } else {
       scrollUp.removeClass('scroll-up--show');
@@ -37,4 +37,79 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop:0}, '300');
   });
 
+  const sliderTop = $('.swiper-container');
+  const slideBot = $('.swiper-containerbot');
+
+  var mySwiper = new Swiper (sliderTop, {
+    loop: true,
+    spaceBetween: 15,
+    pagination: {
+      el: '.top',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var mySwiperBot = new Swiper (slideBot, {
+    loop: true,
+    slaidersPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: '.pagination-bot',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var btnRight = $('.swiper-button-next');
+  var btnLeft = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination-bullets');
+  var bullet = $('.swiper-pagination-bullet');
+  var bulletBot = $('.bot');
+  var btnBotR = $('.btnBotR');
+  var btnBotL = $('.btnBotL');
+  var padBot = $('.pagination-bot');
+
+  bullet.css('margin-right', 16);
+  bulletBot.css('margin-right', 20);
+  bulletBot.css('bottom', 100);
+
+  bulletBot.css('left', btnLeft.width() + 40);
+  btnRight.css('left', btnLeft.width() + 26 + bullets.width() + 13);
+  bullets.css('left', btnLeft.width() + 26);
+  btnBotR.css('left', btnBotL.width() + 26 + bullets.width()*2 + 13);
+
+  padBot.css('top', 300);
+  // Очень страшный кадавр отвечающий за переключенеи фокуса титулов
+  var titlesList = $('.slaid-titles__box');
+  var index = 1;
+  $(document).click(function (e){
+    if(index == 6){
+      index = 0;
+    }
+    if(index == -5){
+      index = 1;
+    }
+    if (btnLeft.is(e.target)){
+      $(index--);
+      var element = titlesList.get(index);
+      var elementLast = titlesList.get((index) - 1);
+      $(elementLast).addClass('slaid-titles__box--light');
+      $(element).removeClass('slaid-titles__box--light');
+    }
+    if (btnBotR.is(e.target)){ 
+      var element = titlesList.get(index);
+      var elementLast = titlesList.get((index) - 1);
+      $(index++);
+      $(element).addClass('slaid-titles__box--light');
+      $(elementLast).removeClass('slaid-titles__box--light');
+    }
+    console.log(index);
+	});
 });
